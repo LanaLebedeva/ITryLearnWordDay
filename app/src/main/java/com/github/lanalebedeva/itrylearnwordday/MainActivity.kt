483cac8dcc4651a9c4c0894757e5a7f8d6ec32b4
lanalebedeva <batetnikova@mail.ru>
2022-12-28 16:40:10 +0300

    add recycleView part2 :)

---
+++ app/src/main/java/com/github/lanalebedeva/itrylearnwordday/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.lanalebedeva.itrylearnwordday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view_tag, WordListFragment())
+            .commit()
+    }
+}
